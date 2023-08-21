475841aba205f7f54c5c41eaa16afff1c99d727c
Santosh Prajapati <santosh52628@gmail.com>
2023-08-22 04:30:58 +0530

    Delete 2_temp.py

--- 2_temp.py
+++
@@ -1,8 +0,0 @@
-import os
-from os import listdir
-path=r'C:\Users\santo\Documents\GitHub\UAS_software\images'
-for images in os.listdir(path):
- 
-    # check if the image ends with png
-    if (images.endswith(".png")):
-        print(images)
